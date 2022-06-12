323598b4c6bfd78da9c0dfdd431a1343d49c0056
networkjourney <62839860+networkjourney@users.noreply.github.com>
2022-06-12 21:58:14 +0530

    Initial commit

---
+++ test1.py
@@ -0,0 +1,23 @@
+#python theory
+#print
+#comment
+#variable
+#open
+#input()
+#conditions = loging building
+
+x = 10
+y = 20
+z = 20
+if y < x and x > z:
+    print("y is greater")
+elif z == y:
+    print("y equal to z")
+elif z == y:
+    print("y equal to z")
+elif z == y:
+    print("y equal to z")
+elif z == y:
+    print("y equal to z")
+else:
+    print("x is now greater")
